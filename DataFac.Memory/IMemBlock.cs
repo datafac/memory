e60a7d45a487bd82400b135e3840ac45e1ba13ed
Psiman62 <psiman-hju@bytz.me>
2026-02-01 13:09:59 +1000

    added ByteBufferWriter

--- DataFac.Memory/IMemBlock.cs
+++ DataFac.Memory/IMemBlock.cs
@@ -1,19 +1,20 @@
 ﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
 
-namespace DataFac.Memory
+namespace DataFac.Memory;
+
+public interface IMemBlock
 {
-    public interface IMemBlock
-    {
-        int BlockSize { get; }
-        bool IsEmpty { get; }
-        bool TryRead(ReadOnlySpan<byte> source);
-        bool TryWrite(Span<byte> target);
-        void WriteTo(Span<byte> target);
-        void WriteTo(int start, int length, Span<byte> target);
-        string ToBase64String();
-        string ToBase64String(int start, int length);
-        string UTF8String { get; set; }
+    int BlockSize { get; }
+    bool IsEmpty { get; }
+    bool TryRead(ReadOnlySpan<byte> source);
+    bool TryWrite(Span<byte> target);
+    void WriteTo(Span<byte> target);
+    void WriteTo(int start, int length, Span<byte> target);
+    string ToBase64String();
+    string ToBase64String(int start, int length);
+    string UTF8String { get; set; }
 
-        // todo ByteString
-    }
+    // todo ByteString
 }
