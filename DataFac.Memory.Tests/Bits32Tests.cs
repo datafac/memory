a76d9dd49f3d20607b7ec600a1872044a488009d
Psiman62 <psiman-hju@bytz.me>
2026-02-23 20:31:43 +1000

    added Bits32 type

---
+++ DataFac.Memory.Tests/Bits32Tests.cs
@@ -0,0 +1,37 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace DataFac.Memory.Tests
+{
+    public class Bits32Tests
+    {
+        [Theory]
+        [InlineData(default(uint), "0x00000000")]
+        [InlineData(1, "0x00000001")]
+        [InlineData(uint.MaxValue, "0xFFFFFFFF")]
+        public void InitBits(uint data, string expected)
+        {
+            Bits32 bits = new Bits32(data);
+            bits.ToString().ShouldBe(expected);
+        }
+
+        [Theory]
+        [InlineData(0x00000000, 0,  true, 0x00000001)]
+        [InlineData(0x00000001, 1,  true, 0x00000003)]
+        [InlineData(0x00000003, 2,  true, 0x00000007)]
+        [InlineData(0x00000007, 3,  true, 0x0000000F)]
+        [InlineData(0x0000000F, 31, true, 0x8000000F)]
+        [InlineData(0x8000000F, 30, true, 0xC000000F)]
+        [InlineData(0xC000000F, 29, true, 0xE000000F)]
+        [InlineData(0xE000000F, 28, true, 0xF000000F)]
+        [InlineData(0xF000000F, 0, false, 0xF000000E)]
+        [InlineData(0xF000000E, 1, false, 0xF000000C)]
+        [InlineData(0xF000000C, 2, false, 0xF0000008)]
+        [InlineData(0xF0000008, 3, false, 0xF0000000)]
+        public void SetBits(uint init, int pos, bool value, uint expected)
+        {
+            Bits32 bits = (new Bits32(init)).SetBit(pos, value);
+            bits.Data.ShouldBe(expected);
+        }
+    }
+}
---
+++ DataFac.Memory/Bits32.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DataFac.Memory
+{
+    public readonly struct Bits32 : IEquatable<Bits32>
+    {
+        public readonly UInt32 Data;
+        public Bits32(UInt32 data) => Data = data;
+        public Bits32(Bits32 other) => Data = other.Data;
+        public bool Equals(Bits32 other) => Data == other.Data;
+        public override bool Equals(object? obj) => obj is Bits32 other && Equals(other);
+        public override int GetHashCode() => HashCode.Combine(Data);
+        override public string ToString() => $"0x{Data:X8}";
+        public static bool operator ==(Bits32 left, Bits32 right) => left.Equals(right);
+        public static bool operator !=(Bits32 left, Bits32 right) => !left.Equals(right);
+
+        public bool GetBit(int index)
+        {
+            if (index < 0 || index > 31) throw new ArgumentOutOfRangeException(nameof(index), "Index must be between 0 and 31.");
+            UInt32 mask = 1u << index;
+            return (Data & mask) != 0;
+        }
+
+        public Bits32 SetBit(int index, bool value)
+        {
+            if (index < 0 || index > 31) throw new ArgumentOutOfRangeException(nameof(index), "Index must be between 0 and 31.");
+            UInt32 mask = 1u << index;
+            UInt32 newData = value ? (Data | mask) : (Data & ~mask);
+            return new Bits32(newData);
+        }
+    }
+}
